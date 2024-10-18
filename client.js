9343485427d53990ce37ad2580513bf5674c9734
keonsoonpark4798 <98qkrrjstns@gmail.com>
2024-10-18 20:58:19 +0900

    패킷구조까지

--- client.js
+++ client.js
@@ -1,5 +1,5 @@
 import net from 'net';
-
+import { writeHeader, readHeader } from './utils.js';
 // 서버에 연결할 호스트와 포트
 const HOST = '127.0.0.1';
 const PORT = 5555;
@@ -9,20 +9,34 @@ const client = new net.Socket();
 client.connect(PORT, HOST, () => {
   console.log('Connected to server');
 
-  const lengthBuffer = Buffer.alloc(4);
-  lengthBuffer.writeInt32BE(data.length + 4, 0);
-  // 직렬화 스트링 -> 바이트 배열
-  const message = 'Hello';
-  const data = Buffer.from(message);
+  //   const lengthBuffer = Buffer.alloc(4);
+  //   lengthBuffer.writeInt32BE(data.length + 4, 0);
+  //   // 직렬화 스트링 -> 바이트 배열
+  //   const message = 'Hello';
+  //   const data = Buffer.from(message);
 
-  const buffer = Buffer.concat([lengthBuffer, data]);
-  client.write(buffer); // write 메서드로 버퍼 쓰기
+  //   const buffer = Buffer.concat([lengthBuffer, data]);
+  //   client.write(buffer); // write 메서드로 버퍼 쓰기
+  const message = 'Hello';
+  const buffer = Buffer.from(message);
+
+  const header = writeHeader(buffer.length, 10);
+  const packet = Buffer.concat([header, buffer]);
+  client.write(packet);
 
   //client.destroy();
 });
 
 client.on('data', (data) => {
-  console.log(data);
+  const { handlerId, length } = readHeader(data);
+  console.log(`handlerId: ${handlerId}`);
+  console.log(`length: ${length}`);
+
+  const headerSize = TOTAL_LENGTH_SIZE + HANDLER_ID;
+  // 메시지 추출
+  const message = buffer.slice(headerSize); // 앞의 헤더 부분을 잘라낸다.
+
+  console.log(`server 에게 받은 메세지: ${message}`);
 });
 
 client.on('close', () => {
